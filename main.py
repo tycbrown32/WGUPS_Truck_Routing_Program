d997de391762f3840dd5cdd5341b8e9f3d49a272
Tyler Brown <tycbrown32@gmail.com>
2023-07-21 15:24:40 -0600

    Phase 3.2 commit

--- main.py
+++ main.py
@@ -16,13 +16,16 @@ class Package:
         self.mass = mass_kg
         self.notes = notes
         self.delivery_status = "AT HUB"
-        self.status_update_time = "Waiting delivery.."
+        self.time_left_hub = "7:00"
+        self.truck = "No Truck"
+        self.delivery_time = "8:00"
 
     def __str__(self):  # overwriting print(Package)
-        return "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.package_id, self.delivery_address, self.delivery_city,
-                                                           self.delivery_state, self.delivery_zipcode,
-                                                           self.delivery_deadline, self.mass, self.notes,
-                                                           self.delivery_status, self.status_update_time)
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s" % (self.package_id, self.delivery_address,
+                                                                   self.delivery_city, self.delivery_state,
+                                                                   self.delivery_zipcode, self.delivery_deadline,
+                                                                   self.mass, self.notes, self.delivery_status,
+                                                                   self.time_left_hub, self.truck, self.delivery_time)
 
 
 # Truck Class ----------------------------------------------------------------------------------------------------------
@@ -36,47 +39,50 @@ class Truck:
         self.speed = speed
         self.distance_travelled = float("%0.2f" % 0)
 
+    # Method to singularly 'load' packages to truck.
     def add_package(self, pack_id):
         if len(self.packages) >= self.pack_limit:
             print('Truck is full!')
         else:
             self.packages.append(pack_id)
 
+    # Method for loading truck given a list of packages. Also updates package objects.
     def load_packages_onto_truck(self, packs):
         for pack in packs:
             self.add_package(pack)
             package_hash.search(pack).delivery_status = "IN ROUTE"
-            package_hash.search(pack).status_update_time = self.truck_time.strftime("%H:%M")
+            package_hash.search(pack).time_left_hub = self.truck_time.strftime("%H:%M")
+            package_hash.search(pack).truck = 'Truck ' + str(self.id)
 
     # Package delivery algorithm using nearest neighbor. Runtime O(n) = xx. Space Complexity O(n) = xx.
     def package_delivery(self):
-        avg_speed = self.speed / 60  # Average speed in miles per minute
+        avg_speed = self.speed / 60  # Average speed in miles per minute.
 
         # Determine the next address and associated distance
-        curr_address = address_lookup('4001 S 700 East', address_list)  # Always start package delivery from HUB
+        curr_address = address_lookup('4001 S 700 East', address_list)  # Always start package delivery from hub.
 
-        # Loop through package list until list is empty
+        # Loop through package list until list is empty.
         while len(self.packages) > 0:
-            short_distance = float("inf")  # Next distance will always be less than infinity so next address will update
+            short_distance = float("inf")  # Next distance will always be less than 'inf' so next address will update.
 
-            # Determine the package with the nearest delivery location
-            package_index = 0
+            # Determine the package with the nearest delivery location.
+            pack_index = 0
             for package in self.packages:
-                tmp_next_address = address_lookup(package_hash.search(int(self.packages[package_index])).delivery_address,
+                new_next_address = address_lookup(package_hash.search(int(self.packages[pack_index])).delivery_address,
                                                   address_list)
-                tmp_distance = float(distance_array[tmp_next_address][curr_address])
+                new_distance = float(distance_array[new_next_address][curr_address])
 
-                # If the temp_distance is less than the current short_distance -> update short_distance
-                if tmp_distance < short_distance:
-                    next_address = tmp_next_address
-                    short_distance = tmp_distance
+                # If the temp_distance is less than the current short_distance -> update short_distance.
+                if new_distance < short_distance:
+                    next_address = new_next_address
+                    short_distance = new_distance
                     remove_package = package
 
-                package_index += 1  # Iterate to next package to determine distance from current location
+                pack_index += 1  # Iterate to next package to determine distance from current location.
 
             # Travel to next destination. Update current address (with next_address).
-            travel_time = timedelta(minutes=(short_distance / avg_speed))  # Calculate travel time (minutes)
-            curr_address = next_address  # Update current address
+            travel_time = timedelta(minutes=(short_distance / avg_speed))  # Calculate travel time (minutes).
+            curr_address = next_address  # Update current address.
             self.truck_time = self.truck_time + travel_time
 
             # 'Deliver' package by removing it from the truck's package list and timestamping it.
@@ -86,7 +92,7 @@ class Truck:
 
             self.packages.remove(remove_package)
             package_hash.search(remove_package).delivery_status = "DELIVERED"
-            package_hash.search(remove_package).status_update_time = self.truck_time.strftime('%H:%M')
+            package_hash.search(remove_package).delivery_time = self.truck_time.strftime('%H:%M')
             self.distance_travelled = float("%0.2f" % (self.distance_travelled + short_distance))
 
             """print('Truck package list: ', self.packages)
@@ -95,9 +101,10 @@ class Truck:
 
         # 'Return to hub'. Update distance travelled by truck and time truck returned to hub.
         """print('\nReturning to HUB.')"""
-        distance_to_hub = float(distance_array[0][curr_address])  # Calculate distance back to hub
-        self.truck_time = self.truck_time + timedelta(minutes=(distance_to_hub / avg_speed))  # Calculate and update time returned to hub
-        self.distance_travelled = self.distance_travelled + distance_to_hub  # Calculate distance after return to HUB
+        distance_to_hub = float(distance_array[0][curr_address])  # Calculate distance back to hub.
+        time_to_hub = distance_to_hub / avg_speed  # Calculate time to return to hub (minutes).
+        self.truck_time = self.truck_time + timedelta(minutes=time_to_hub)  # Calculate and update time returned to hub.
+        self.distance_travelled = self.distance_travelled + distance_to_hub  # Calculate distance after return to hub.
         """print('Total distance travelled was ', self.distance_travelled, ' miles. Current time is ',
               self.truck_time.strftime('%H:%M'), '.\n\n')"""
 
@@ -107,29 +114,30 @@ class Truck:
 
 # Chaining Hash Table Class---------------------------------------------------------------------------------------------
 class ChainingHashTable:
+    # Initialize hash table.
     def __init__(self, initial_capacity=10):
         self.table = []
         for i in range(initial_capacity):
             self.table.append([])
 
-    # Insert new item into hash table
+    # Insert new object into hash table.
     def insert(self, key, obj):
-        # get bucket list where val will go
+        # Get bucket list where obj will be inserted.
         bucket = hash(key) % len(self.table)
         bucket_list = self.table[bucket]
 
-        # Update bucket_list information if key already exists
+        # Update bucket_list information if key already exists.
         for kv in bucket_list:
             if kv[0] == key:
                 kv[1] = obj
                 return True
 
-        # Insert val to end of bucket list
+        # Insert key_value to end of bucket list.
         key_value = [key, obj]
         bucket_list.append(key_value)
         return True
 
-    # Search for item in hash table
+    # Search for and return object from hash table.
     def search(self, key):
         bucket = hash(key) % len(self.table)
         bucket_list = self.table[bucket]
@@ -139,7 +147,7 @@ class ChainingHashTable:
                 return kv[1]
         return None
 
-    # Remove item from hash table
+    # Remove object from hash table.
     def remove(self, key):
         bucket = hash(key) % len(self.table)
         bucket_list = self.table[bucket]
@@ -151,11 +159,12 @@ class ChainingHashTable:
         return None
 
 
-package_hash = ChainingHashTable()  # Create hash table
+package_hash = ChainingHashTable()  # Define a hash table.
+package_hash_key = []  # List of keys for the hash table. Provides a method for iterating the hash table.
 
 
 # Function to load data from .csv file into Package objects-------------------------------------------------------------
-def load_package_data(csv_file, hash_table):
+def load_package_data(csv_file, hash_table, hash_key):
     # https://docs.python.org/3/library/csv.html
     with open(csv_file, newline='') as packages_csv:
         package_data = csv.reader(packages_csv, delimiter=',', quotechar='|')
@@ -163,9 +172,10 @@ def load_package_data(csv_file, hash_table):
         for row in package_data:
             package_obj = Package(int(row[0]), row[1], row[2], row[3], row[4], row[5], row[6], row[7])
             hash_table.insert(int(package_obj.package_id), package_obj)
+            hash_key.append(int(row[0]))
 
 
-load_package_data('packages.csv', package_hash)  # Load package data from .csv file
+load_package_data('packages.csv', package_hash, package_hash_key)  # Load package data from .csv file.
 
 """
 # Get data from hash table
@@ -185,7 +195,7 @@ def create_distance_array(csv_file):
         return distance_arr
 
 
-distance_array = create_distance_array('distances.csv')  # Create array of distances
+distance_array = create_distance_array('distances.csv')  # Create array of distances.
 
 
 # Create address list from .csv file -----------------------------------------------------------------------------------
@@ -199,7 +209,7 @@ def create_address_list(csv_file):
         return list_addresses
 
 
-address_list = create_address_list('addresses.csv')  # Create address list
+address_list = create_address_list('addresses.csv')  # Create address list.
 
 
 # Address lookup function to determine a number associated with and address --------------------------------------------
@@ -211,17 +221,45 @@ def address_lookup(address_str, list_addresses):
                 return row_count
         row_count += 1
 
-"""
-def delivery_time(check_time):
-    for pack in package_hash:
-        if package_hash.search(pack).delivery_status == "DELIVERED" and
-            package_hash.search(pack).status_update_time >= check_time:
-"""
+
+# Function for determine package status a specified time ---------------------------------------------------------------
+def package_status(check_time, hash_table, hash_table_key):
+    for pack in hash_table_key:
+        delivery_time_str = hash_table.search(pack).delivery_time
+        delivery_time = time_correction(delivery_time_str)
+
+        time_left_hub_str = hash_table.search(pack).time_left_hub
+        time_left_hub = time_correction(time_left_hub_str)
+
+        if delivery_time <= check_time:
+            print('Package ', pack, ' delivered.')
+        elif time_left_hub <= check_time:
+            print('Package ', pack, ' in route.')
+        else:
+            print('Package ', pack, ' at Hub.')
+
+
+# Function to adjust a time to "Today" when given hour and minute string inputs ----------------------------------------
+def time_correction(time_str):
+    time_hour = datetime.strptime(time_str, "%H:%M").hour
+    time_minute = datetime.strptime(time_str, "%H:%M").minute
+
+    curr_year = datetime.today().year
+    curr_month = datetime.today().month
+    curr_day = datetime.today().day
+
+    corrected_time = datetime(curr_year, curr_month, curr_day, time_hour, time_minute)
+    return corrected_time
 
 
 # WGUPS Operation execution --------------------------------------------------------------------------------------------
-truck1_start_time = datetime(2023, 7, 19, 8, 0)  # truck1 starts its first delivery at 8:00 AM
-truck2_start_time = datetime(2023, 7, 19, 9, 5)  # truck2 starts its first delivery at 9:05 AM when delayed packages arrive
+# Establish today's month, day, and year.
+month = datetime.today().month
+day = datetime.today().day
+year = datetime.today().year
+
+truck1_start_time = datetime(year, month, day, 8, 0)  # truck1 starts first delivery at 8:00AM
+truck2_start_time = datetime(year, month, day, 9, 5)  # truck2 starts first delivery at 9:05AM (delayed packages arrive)
 
 tp1 = [13, 39, 14, 15, 16, 34, 19, 20, 21, 29, 7, 30, 8, 31, 32, 6]
 tp2 = [2, 33, 10, 11, 12, 17, 22, 23, 24]
@@ -244,21 +282,14 @@ truck1.package_delivery()
 truck2.load_packages_onto_truck(tp4)
 truck2.package_delivery()
 
+# Ask user for time to view package status. Convert time string to datetime
+req_time_str = input("Enter time to check package delivery status (H:M, 24-hour): ")
+req_time = time_correction(req_time_str)
 
-req_time = input("Enter time to check package delivery status: ")
+package_status(req_time, package_hash, package_hash_key)
 
-print('Package 1 delivered at ', package_hash.search(1).status_update_time, '\n',
-      'Package 6 delivered at ', package_hash.search(6).status_update_time, '\n',
-      'Package 13 delivered at ', package_hash.search(13).status_update_time, '\n',
-      'Package 14 delivered at ', package_hash.search(14).status_update_time, '\n',
-      'Package 15 delivered at ', package_hash.search(15).status_update_time, '\n',
-      'Package 16 delivered at ', package_hash.search(16).status_update_time, '\n',
-      'Package 20 delivered at ', package_hash.search(20).status_update_time, '\n',
-      'Package 25 delivered at ', package_hash.search(25).status_update_time, '\n',
-      'Package 29 delivered at ', package_hash.search(29).status_update_time, '\n',
-      'Package 30 delivered at ', package_hash.search(30).status_update_time, '\n',
-      'Package 31 delivered at ', package_hash.search(31).status_update_time, '\n',
-      'Package 34 delivered at ', package_hash.search(34).status_update_time, '\n',
-      'Package 37 delivered at ', package_hash.search(37).status_update_time, '\n',
-      'Package 40 delivered at ', package_hash.search(40).status_update_time, '\n')
-print('Total distance travelled is', float("%0.2f" % (truck1.distance_travelled + truck2.distance_travelled)), 'miles.')
+print('\nTotal distance travelled is', float("%0.2f" % (truck1.distance_travelled + truck2.distance_travelled)),
+      'miles.\n')
+
+for item in package_hash_key:
+    print('Package ', item, ': ', package_hash.search(item))
