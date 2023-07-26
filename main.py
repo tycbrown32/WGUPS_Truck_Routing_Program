ae09392bef7b1d60a4a6c63a05cbaa8aaa6f03b6
Tyler Brown <tycbrown32@gmail.com>
2023-07-26 17:12:02 -0600

    Phase 4 commit

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ class Truck:
         self.distance_travelled = float("%0.2f" % 0)
 
     # Method to singularly 'load' packages to truck.
+    # Runtime complexity = O(1). Space Complexity = O(1)
     def add_package(self, pack_id):
         if len(self.packages) >= self.pack_limit:
             print('Truck is full!')
@@ -47,6 +48,7 @@ class Truck:
             self.packages.append(pack_id)
 
     # Method for loading truck given a list of packages. Also updates package objects.
+    # Runtime complexity = O(n^2). Space Complexity = O(n)
     def load_packages_onto_truck(self, packs):
         for pack in packs:
             self.add_package(pack)
@@ -54,7 +56,8 @@ class Truck:
             package_hash.search(pack).time_left_hub = self.truck_time.strftime("%H:%M")
             package_hash.search(pack).truck = 'Truck ' + str(self.id)
 
-    # Package delivery algorithm using nearest neighbor. Runtime O(n) = xx. Space Complexity O(n) = xx.
+    # Package delivery algorithm using nearest neighbor.
+    # Runtime = O(n^3). Space Complexity = O(n).
     def package_delivery(self):
         avg_speed = self.speed / 60  # Average speed in miles per minute.
 
@@ -76,7 +79,7 @@ class Truck:
                 if new_distance < short_distance:
                     next_address = new_next_address
                     short_distance = new_distance
-                    remove_package = package
+                    removal_package = package  # ID of package to be removed
 
                 pack_index += 1  # Iterate to next package to determine distance from current location.
 
@@ -86,33 +89,23 @@ class Truck:
             self.truck_time = self.truck_time + travel_time
 
             # 'Deliver' package by removing it from the truck's package list and timestamping it.
-            """ print('Heading to address ', package_hash.search(next_address).delivery_address, ', ', short_distance,
-                  ' miles. Delivering package ', remove_package, '. Package delivered at ',
-                  self.truck_time.strftime('%H:%M'), '.')"""
-
-            self.packages.remove(remove_package)
-            package_hash.search(remove_package).delivery_status = "DELIVERED"
-            package_hash.search(remove_package).delivery_time = self.truck_time.strftime('%H:%M')
+            self.packages.remove(removal_package)
+            package_hash.search(removal_package).delivery_status = "DELIVERED"
+            package_hash.search(removal_package).delivery_time = self.truck_time.strftime('%H:%M')
             self.distance_travelled = float("%0.2f" % (self.distance_travelled + short_distance))
 
-            """print('Truck package list: ', self.packages)
-            print('Current address is ', curr_address, '. Total distance travelled is ',
-                self.distance_travelled, ' miles.\n')"""
-
         # 'Return to hub'. Update distance travelled by truck and time truck returned to hub.
-        """print('\nReturning to HUB.')"""
         distance_to_hub = float(distance_array[0][curr_address])  # Calculate distance back to hub.
         time_to_hub = distance_to_hub / avg_speed  # Calculate time to return to hub (minutes).
         self.truck_time = self.truck_time + timedelta(minutes=time_to_hub)  # Calculate and update time returned to hub.
         self.distance_travelled = self.distance_travelled + distance_to_hub  # Calculate distance after return to hub.
-        """print('Total distance travelled was ', self.distance_travelled, ' miles. Current time is ',
-              self.truck_time.strftime('%H:%M'), '.\n\n')"""
 
     def __str__(self):  # overwriting print(Truck)
         return "%s, %s, %s, %s" % (self.id, self.location, self.truck_time.strftime("%H:%M"), self.distance_travelled)
 
 
 # Chaining Hash Table Class---------------------------------------------------------------------------------------------
+# Runtime complexity = O(n). Space Complexity = O(n)
 class ChainingHashTable:
     # Initialize hash table.
     def __init__(self, initial_capacity=10):
@@ -164,6 +157,7 @@ package_hash_key = []  # List of keys for the hash table. Provides a method for 
 
 
 # Function to load data from .csv file into Package objects-------------------------------------------------------------
+# Runtime complexity = O(n). Space Complexity = O(n)
 def load_package_data(csv_file, hash_table, hash_key):
     # https://docs.python.org/3/library/csv.html
     with open(csv_file, newline='') as packages_csv:
@@ -177,14 +171,9 @@ def load_package_data(csv_file, hash_table, hash_key):
 
 load_package_data('packages.csv', package_hash, package_hash_key)  # Load package data from .csv file.
 
-"""
-# Get data from hash table
-for i in range(len(package_hash.table) * 4):
-    print("Key: {} and Package {}".format(i + 1, package_hash.search(i + 1)))
-"""
-
 
 # Create distance array and address list from .csv file ----------------------------------------------------------------
+# Runtime complexity = O(n). Space Complexity = O(n)
 def create_distance_array(csv_file):
     with open(csv_file, newline='') as distances_csv:
         distances = csv.reader(distances_csv, delimiter=',', quotechar='|')
@@ -199,6 +188,7 @@ distance_array = create_distance_array('distances.csv')  # Create array of dista
 
 
 # Create address list from .csv file -----------------------------------------------------------------------------------
+# Runtime complexity = O(n). Space Complexity = O(n)
 def create_address_list(csv_file):
     with open(csv_file, newline='') as addresses_csv:
         addresses = csv.reader(addresses_csv, delimiter=',', quotechar='|')
@@ -212,7 +202,8 @@ def create_address_list(csv_file):
 address_list = create_address_list('addresses.csv')  # Create address list.
 
 
-# Address lookup function to determine a number associated with and address --------------------------------------------
+# Address lookup function: provides a number to be associated with an address ------------------------------------------
+# Runtime complexity = O(n^2). Space Complexity = O(n^2)
 def address_lookup(address_str, list_addresses):
     row_count = 0
     for row in list_addresses:
@@ -222,7 +213,8 @@ def address_lookup(address_str, list_addresses):
         row_count += 1
 
 
-# Function for determine package status a specified time ---------------------------------------------------------------
+# Function to determine package status at a specified time -------------------------------------------------------------
+# Runtime complexity = O(n^2). Space Complexity = O(n).
 def package_status(check_time, hash_table, hash_table_key):
     for pack in hash_table_key:
         delivery_time_str = hash_table.search(pack).delivery_time
@@ -240,6 +232,7 @@ def package_status(check_time, hash_table, hash_table_key):
 
 
 # Function to adjust a time to "Today" when given hour and minute string inputs ----------------------------------------
+# Runtime complexity = O(1). Space Complexity = O(1)
 def time_correction(time_str):
     time_hour = datetime.strptime(time_str, "%H:%M").hour
     time_minute = datetime.strptime(time_str, "%H:%M").minute
@@ -266,7 +259,7 @@ tp2 = [2, 33, 10, 11, 12, 17, 22, 23, 24]
 tp3 = [4, 40, 1, 25, 26, 38, 5, 37]
 tp4 = [3, 18, 36, 28, 35, 27, 9]
 
-print("DELIVERING:")
+# Begin delivery process.
 truck1 = Truck(1, truck1_start_time)
 truck2 = Truck(2, truck2_start_time)
 
@@ -282,14 +275,36 @@ truck1.package_delivery()
 truck2.load_packages_onto_truck(tp4)
 truck2.package_delivery()
 
-# Ask user for time to view package status. Convert time string to datetime
-req_time_str = input("Enter time to check package delivery status (H:M, 24-hour): ")
-req_time = time_correction(req_time_str)
-
-package_status(req_time, package_hash, package_hash_key)
-
-print('\nTotal distance travelled is', float("%0.2f" % (truck1.distance_travelled + truck2.distance_travelled)),
+print('\nTotal distance travelled was', float("%0.2f" % (truck1.distance_travelled + truck2.distance_travelled)),
       'miles.\n')
 
-for item in package_hash_key:
-    print('Package ', item, ': ', package_hash.search(item))
+# User interface -------------------------------------------------------------------------------------------------------
+# Runtime complexity = O(n^2). Space Complexity = O(n)
+next_step = 'm'  # Initialize variable to go to the 'main menu'
+while next_step != 'e':
+    check = input("\nCheck delivery status (d) OR view package information (v) OR exit (e): ")
+
+    if check == 'd':
+        # Ask user for time to view package status. Convert time string to datetime
+        req_time_str = input("\nEnter time to check package delivery status (H:M, 24-hour): ")
+        req_time = time_correction(req_time_str)
+
+        package_status(req_time, package_hash, package_hash_key)
+    elif check == 'v':
+        packageID = input("\nEnter package ID for package info  or 'all' for info of all packages. ")
+        print('')
+
+        if packageID == 'all':  # Print information for all packages.
+            for item in package_hash_key:
+                print('Package ', item, ': ', package_hash.search(item))
+        else:  # Print information for provided package id.
+            print('Package ', packageID, ': ', package_hash.search(int(packageID)))
+    else:
+        next_step = 'e'
+        print("\nExiting application.")
+        break
+
+    next_step = input("\nReturn to main menu (m) OR exit (e)? ")
+    if next_step == 'e':
+        print("\nExiting application.")
+
